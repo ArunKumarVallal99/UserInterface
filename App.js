162ea20873acddc8fe82559be764ba071a41d2f2
Arunkumarvallal99 <arunkumar.v@optisolbusiness.com>
2020-11-25 17:47:35 +0530

    TouchableHighlight

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 
 import React, { useState } from 'react';
-import { StyleSheet, StatusBar,Image,Text, TextInput, View, Button, ScrollView, SafeAreaView,Alert, TouchableOpacity, Switch, KeyboardAvoidingView } from 'react-native';
+import { StyleSheet, StatusBar,Image,Text, TextInput, View, Button, ScrollView, SafeAreaView,Alert, TouchableOpacity, Switch, KeyboardAvoidingView, TouchableHighlight } from 'react-native';
 
 const yourApp= () => {
   const [isOn,toggled]=useState(false);
@@ -27,6 +27,10 @@ const yourApp= () => {
             />
           </View>
 
+          <TouchableHighlight style={styles.touchableHighlight} onPress={()=>Alert.alert('You Have Pressed The Button')}>
+            <Text style={styles.loginText}>Login</Text>
+              </TouchableHighlight>
+
           <Text style={styles.bottomText}>HI</Text>
 
         </View>
@@ -47,11 +51,20 @@ const styles = StyleSheet.create({
   },
   inputTextView:{
     paddingTop:300,
+    paddingBottom:20,
   },
   inputText:{
     width:100,
     borderWidth:4,
     height:50,
+  },
+  touchableHighlight:{
+    backgroundColor:'blue',
+    width:150,
+    height:40,
+    borderRadius:5,
+    justifyContent:'center',
+    alignItems:'center',
   },
   bottomText:{
     paddingTop:100,
