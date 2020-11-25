c4d4d9915d5dd93ca2fc810f608ea6d0536d01a1
Arunkumarvallal99 <arunkumar.v@optisolbusiness.com>
2020-11-25 14:44:56 +0530

    Switch Commit

--- App.js
+++ App.js
@@ -1,28 +1,19 @@
 
-import React from 'react';
-import { StyleSheet, StatusBar,Image,Text, TextInput, View, Button, ScrollView, SafeAreaView,Alert, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, StatusBar,Image,Text, TextInput, View, Button, ScrollView, SafeAreaView,Alert, TouchableOpacity, Switch } from 'react-native';
 
 const yourApp= () => {
-  const onPress = () => {
-    Alert.alert('Please Wait');
-  }
+  const [isOn,toggled]=useState(false);
+  const switcher =()=> toggled(previousState=>!previousState);
 
  
   return (
    
       <View style={styles.container}>
-        <View style={styles.touch}>
-          <TouchableOpacity style={styles.touchButton} onPress={onPress}>
-            <Text style={styles.text}>Click Here</Text>
-          </TouchableOpacity>
-        </View>
-
-        <View styles={styles.button}>
-          <Button
-              title="Login"
-              color="green"
-              onPress={() => Alert.alert('Please Wait')}/>
-        </View>
+       <Switch value={isOn}
+       onValueChange={switcher}
+       trackColor={{false:'grey',true:'black'}}
+       thumbColor={isOn ? 'red' :'white'}/>
       </View>
      
   );
@@ -34,24 +25,7 @@ const styles = StyleSheet.create({
    alignItems:'center',
    flex:1,
  },
-touch:{
-  paddingBottom:40,
-},
- touchButton:{
-   alignItems:'center',
-   width:100,
-   height:25,
-   backgroundColor:'blue',
-   borderRadius:20,
- },
- button:{
-   marginTop:20,
-   paddingTop:20,
- },
- text:{
-   color:'white',
-   fontWeight:'bold',
- }
+
 });
 
 export default yourApp;
--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ## Button and TouchableOpacity
 
 <img src="https://github.com/ArunKumarVallal99/UserInterface/blob/master/ScreenShots/Screenshot_1606293174.png" height="300">
+
+## Switch
+
+<img src="https://github.com/ArunKumarVallal99/UserInterface/blob/master/ScreenShots/Screenshot_1606295637.png" height="300">
